a53c15cbfc13ad0cac50217143dedf963683defe
danyalkhalid <dkhalid354@outlook.com>
2024-02-05 16:52:14 +0000

    AddMuseum

--- museum-react/src/App.js
+++ museum-react/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage'; 
 import Museum from './Museum';
+import AddMuseum from './AddMuseum'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/museums" element={<Museum />} />
+          <Route path="/addMuseum" element={<AddMuseum />} />
         </Routes>
       </div>
     </Router>
