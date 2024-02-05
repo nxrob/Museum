01185c436f19380e3d3d918d564d81ccc4d8c9eb
JoshuaDayan <144036077+JoshuaDayan@users.noreply.github.com>
2024-02-05 11:42:40 +0000

    Docker_Working

--- museum-spring/src/main/java/com/example/Museum/model/Artist.java
+++ museum-spring/src/main/java/com/example/Museum/model/Artist.java
@@ -13,6 +13,54 @@ public class Artist {
     @Id
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDobAndDod() {
+        return dobAndDod;
+    }
+
+    public void setDobAndDod(String dobAndDod) {
+        this.dobAndDod = dobAndDod;
+    }
+
+    public String getBirthplace() {
+        return birthplace;
+    }
+
+    public void setBirthplace(String birthplace) {
+        this.birthplace = birthplace;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public List<Art> getRepertoire() {
+        return repertoire;
+    }
+
+    public void setRepertoire(List<Art> repertoire) {
+        this.repertoire = repertoire;
+    }
+
     private String name;
     private String dobAndDod;
     private String birthplace;
