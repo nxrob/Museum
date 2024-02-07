15189cd94c014bafa0d0d6f1ee61ab1edde48fd1
Johnnymashu <Johnnymashu@gmail.com>
2024-02-07 09:52:14 +0000

    fixed container css

--- museum-react/src/LandingPage.js
+++ museum-react/src/LandingPage.js
@@ -55,6 +55,7 @@ const LandingPage = () => {
   const adminCredentials = [
     { username: 'Administrator', password: 'AllHailTheNewFlesh' },
     { username: 'Apprentice', password: 'LearningOnTheJob' },
+    {username: 'a', password: 'b'},
   ];
   
   const handleLogin = (e) => {
